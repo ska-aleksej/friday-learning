e4b793be12bcd698da515a728109b8afed205eca
Aleksej Skaskevich <aleksej.skaskevich@gmail.com>
2025-04-05 22:14:47 +0300

    Добавлены тесты и запуск

---
+++ src/test/java/ru/fridaylearning/FridayWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fridaylearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridayWordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Тест пройдет успешно, если контекст Spring Boot загрузится без ошибок
+    }
+} 
